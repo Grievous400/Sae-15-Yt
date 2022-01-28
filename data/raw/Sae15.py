ca3e0d4dc4639fd4703c12a4e59d6dab6f43999d
Louis Claudel <louis.claudel1@uha.fr>
2022-01-28 09:05:15 +0100

    1

--- data/raw/Sae15.py
+++ data/raw/Sae15.py
@@ -19,23 +19,7 @@ df.to_csv("fusion.csv", index=False)
 
 print(df.sort_values('8', ascending=False)[:5])
 
-def ComputeMedian():
-    df_median = df['8'].median()
-    print("La moyenne de likes est de :", int(df_median))
 
-    df_median2 = df['9'].median()
-    print("La moyenne de dislikes est de :", int(df_median2))
-    return False
-print(ComputeMedian())
-
-def ComputeMean():
-    df_mean = df['8'].mean()
-    print("La moyenne de likes est de :", int(df_mean))
-
-    df_mean2 = df['9'].median()
-    print("La moyenne de dislikes est de :", int(df_mean2))
-    return False
-print(ComputeMedian())
 
 
 #Moyenne
--- src/model/model.py
+++ src/model/model.py
@@ -1,11 +1,22 @@
- 
+
+data = pd.read_csv('U:\Documents\youtube\Sae-15-yt\data\raw\fusion.csv')
+df = pd.DataFrame(data)
+
+def ComputeMedian():
+    df_median = df['8'].median()
+    print("La médiane de likes est de :", int(df_median))
+
+    df_median2 = df['9'].median()
+    print("La médiane de dislikes est de :", int(df_median2))
+    return False
+print(ComputeMedian())
 
 def ComputeMean():
-    mean = 0
-    return (mean)
+    df_mean = df['8'].mean()
+    print("La moyenne de likes est de :", int(df_mean))
 
-def ComputeMedian:
-    median = 0
-    return (median)    
-    
+    df_mean2 = df['9'].median()
+    print("La moyenne de dislikes est de :", int(df_mean2))
+    return False
+print(ComputeMedian())
     
